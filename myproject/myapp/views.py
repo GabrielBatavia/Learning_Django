216ac72a8bd516ebc5c6a282d699fa1d14309b61
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-02-25 22:37:29 +0700

    Make register.html file and make register function

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
 def index(request):
     features = Feature.objects.all()
     return render(request, 'index.html', {'feature': features})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Your password is incorrect or not same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+           
 
 def counter(request):
     text = request.POST.get('text')
