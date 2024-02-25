ab09915cd5c84e5a1738994f32c0fc482903127b
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-02-25 12:38:11 +0700

    add feature function in views.py and connect to index.html

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    context = {
-        'name': 'Shalom',
-        'age': 23,
-        'nationality': 'Indonesian'
-    }
-    return render(request, 'index.html', context)
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Machine Learning Engginer'
+    feature1.details = 'Deep Learning - Cloud Computing - Database management'
+    return render(request, 'index.html', {'feature': feature1})
 
 def counter(request):
     text = request.POST.get('text')
